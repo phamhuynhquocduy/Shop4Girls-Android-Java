0e798897d1d7192bdadd57a709e51b4cf7cd67e1
nhulm1501 <nhuviolet2000@gmail.com>
2020-11-30 23:53:12 +0700

    design layout hoadon

---
+++ Shop4Girls/app/src/main/java/com/example/shop4girls/view/BillDepositActivity.java
@@ -0,0 +1,16 @@
+package com.example.shop4girls.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shop4girls.R;
+
+public class BillDepositActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_deposit);
+    }
+}
